864f74cf43c6ce9fe519e33b7fcb30929056729c
Jessica <jessica@embrace.mx>
2024-10-20 02:36:52 -0700

    Initial Commit

---
+++ src/DockerBackup.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DockerBackup.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ src/DockerBackup.Model/BackupsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerBackup.Model;
+
+public class BackupsDbContext : IdentityDbContext
+{
+    public BackupsDbContext(DbContextOptions<BackupsDbContext> options): base(options)
+    {
+    }
+}
